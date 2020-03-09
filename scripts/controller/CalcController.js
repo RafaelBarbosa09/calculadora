f3356d87a098fff4fff54401add957185180349b
rbarbosa <rbarbosa@coppetec.coppe.ufrj.br>
2020-03-09 14:15:52 -0300

    teste branches

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -33,7 +33,6 @@ class CalcController {
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 
-
     get displayTime() {
         return this._timeEl.innerHTML;
     }
