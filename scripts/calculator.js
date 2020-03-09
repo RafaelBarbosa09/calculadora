633cd7439e4e3991f460e532c3612f8efff1c1e8
rbarbosa <rbarbosa@coppetec.coppe.ufrj.br>
2020-03-09 14:33:53 -0300

    teste

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,15 +1,1 @@
 window.calculator = new CalcController();
-
-class CalcController {
-    
-    constructor() {
-        
-        this._locale = 'pt-br';
-        this._displayCalcEl = document.getElementById("display");
-        this._dateEl = document.querySelector("#data");
-        this._timeEl = document.querySelector("#hora");
-        this._currentDate; 
-        this.initialize();
-    }
-
-}
